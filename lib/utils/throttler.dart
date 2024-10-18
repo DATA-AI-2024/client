38f13f64e63b8471507b80c99d1a6f3846e68a56
lazyyq <kykint@naver.com>
2024-10-18 15:25:32 +0900

    Feat : Throttler 업데이트

--- lib/utils/throttler.dart
+++ lib/utils/throttler.dart
@@ -1,22 +1,41 @@
+import 'dart:async';
 import 'dart:ui';
 
 class Throttler {
-  Throttler({this.throttleGapInMillis = 1000});
+  Throttler({
+    this.throttleGapInMillis = 1000,
+    this.runLastAttemptedAction = false,
+  });
 
   final int throttleGapInMillis;
 
+  /// After [throttleGapInMillis], run the last action that was attempted to be
+  /// added before [throttleGapInMillis] expired and thus was supposed to be dropped.
+  final bool runLastAttemptedAction;
+
   int? lastActionTime;
 
+  VoidCallback? lastAttemptedAction;
+
+  late final runLastAttemptedActionDuration =
+      Duration(milliseconds: throttleGapInMillis);
+
   void run(VoidCallback action) {
-    if (lastActionTime == null) {
+    final now = DateTime.now();
+
+    if (lastActionTime == null ||
+        now.millisecondsSinceEpoch - lastActionTime! >= throttleGapInMillis) {
       action();
-      lastActionTime = DateTime.now().millisecondsSinceEpoch;
-    } else {
-      if (DateTime.now().millisecondsSinceEpoch - lastActionTime! >=
-          throttleGapInMillis) {
-        action();
-        lastActionTime = DateTime.now().millisecondsSinceEpoch;
+      lastActionTime = now.millisecondsSinceEpoch;
+      if (runLastAttemptedAction) {
+        Timer(runLastAttemptedActionDuration, () {
+          if (lastAttemptedAction != null) {
+            run(lastAttemptedAction!);
+          }
+        });
       }
+    } else {
+      lastAttemptedAction = action;
     }
   }
 }
